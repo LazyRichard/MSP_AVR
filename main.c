7de56c14461826b8131d6aa4aa76c86e9944f519
Seo Junmin <sof.midnight@live.co.kr>
2016-09-20 00:10:05 +0900

    전역 구조체 타입들 types.h로 분리

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include <stdint.h>
 
 #include "msp.h"
+#include "types.h"
 
 #define INPUT 0
 #define OUTPUT 1
@@ -46,46 +47,9 @@
 //#define DEBUG_EXTINT
 
 /*
- * Enum
- */
-enum RC_CHANS {
-	ROLL, PITCH, YAW, THROTTLE,
-	AUX1, AUX2, AUX3 , AUX4 , AUX5 , AUX6 , AUX7,
-	AUX8, AUX9, AUX10, AUX11, AUX12, AUX13, AUX14
-};
-
-enum ATT {
-	ANGX, ANGY, HEADING
-};
-
-enum GPS {
-	LAT, LON
-};
-
-/*
  * 전역 상수
  */
 const uint8_t TCNT2_Value = TCNT2_BASE_1MS;
-
-/*
- * 전역 구조체
- */
-typedef struct _MSP_RC_T {
-	uint16_t rcData[18];
-} msp_rc_t;
-
-typedef struct _MSP_GPS_T {
-	uint8_t fix;
-	uint8_t num_sat;
-	uint32_t gpsData[2];
-	uint16_t altitude;
-	uint16_t speed;
-	uint16_t ground_course;
-} msp_gps_t;
-
-typedef struct _MSP_ATT_T {
-	uint16_t attData[3];
-} msp_att_t;
 
 /*
  * 전역 변수
---
+++ types.h
@@ -0,0 +1,45 @@
+﻿/*
+ * types.h
+ *
+ * Created: 2016-09-20 오전 12:08:09
+ *  Author: SeoJu
+ */ 
+
+#pragma  once
+
+/*
+ * Enum
+ */
+enum RC_CHANS {
+	ROLL, PITCH, YAW, THROTTLE,
+	AUX1, AUX2, AUX3 , AUX4 , AUX5 , AUX6 , AUX7,
+	AUX8, AUX9, AUX10, AUX11, AUX12, AUX13, AUX14
+};
+
+enum ATT {
+	ANGX, ANGY, HEADING
+};
+
+enum GPS {
+	LAT, LON
+};
+
+/*
+ * 전역 타입
+ */
+typedef struct _MSP_RC_T {
+	uint16_t rcData[18];
+} msp_rc_t;
+
+typedef struct _MSP_GPS_T {
+	uint8_t fix;
+	uint8_t num_sat;
+	uint32_t gpsData[2];
+	uint16_t altitude;
+	uint16_t speed;
+	uint16_t ground_course;
+} msp_gps_t;
+
+typedef struct _MSP_ATT_T {
+	uint16_t attData[3];
+} msp_att_t;
